9d209868e0148159fef3bce3a2b0fe6e63b04b67
Toni Schmidbauer <toni@stderr.at>
2014-04-26 22:37:35 +0200

    install rails and postgresql

--- vagrant/puppet/manifests/init.pp
+++ vagrant/puppet/manifests/init.pp
@@ -10,7 +10,30 @@ class railsdev {
   rvm_system_ruby { 'ruby-2.1.1':
     ensure => present,
     default_use => true,
+  } 
+
+  rvm_gemset {
+    'ruby-2.1.1@railsdev':
+      ensure  => present,
+      require => Rvm_system_ruby['ruby-2.1.1'];
+  } 
+
+  rvm_gem {
+    'ruby-2.1.1@railsdev/rails':
+      ensure  => '4.1',
+      require => Rvm_gemset['ruby-2.1.1@railsdev'];
   }  
+
+  require postgresql::server
+  require postgresql::client
+
+  postgresql::server::role { 'railsdev':
+    password_hash => postgresql_password('railsdev', 'railsdev'),
+  } ->
+  postgresql::server::db { 'railsdev':
+    user     => 'railsdev',
+    password => postgresql_password('railsdev', 'railsdev'),
+  } 
 }
 
 include railsdev
