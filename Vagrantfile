43dc7996c42beefdf85b145250fca4ef2d83ec27
Toni Schmidbauer <toni@stderr.at>
2016-07-11 20:51:34 +0200

    provisioning works again, uses newest puppet box

--- Vagrantfile
+++ Vagrantfile
@@ -3,18 +3,18 @@
 
 Vagrant.configure("2") do |config|
 
+  config.ssh.forward_agent = true
+
   config.vm.define "railsdev" do |dev|
-    dev.vm.box = "centos-65-x64-virtualbox-puppet"
+    dev.vm.box = "puppetlabs/centos-7.2-64-puppet"
     dev.vm.hostname = 'railsdev'
-    dev.vm.box_url = "http://puppet-vagrant-boxes.puppetlabs.com/centos-65-x64-virtualbox-puppet.box"
-    dev.vm.network "forwarded_port", guest: 80,   host: 8080
+    dev.vm.network "forwarded_port", guest: 80,   host: 8086
     dev.vm.network "forwarded_port", guest: 443,  host: 8443
     dev.vm.network "forwarded_port", guest: 3000, host: 3000
 
     dev.vm.provision :puppet do |puppet|
-      puppet.manifest_file  = "init.pp"
-      puppet.manifests_path = "vagrant/puppet/manifests"
-      puppet.module_path = "vagrant/puppet/modules"
+      puppet.environment_path = "puppet/environments"
+      puppet.environment = "vagrant"
     end
   end
 end
--- puppet/environments/vagrant/manifests/init.pp
+++ puppet/environments/vagrant/manifests/init.pp
@@ -7,21 +7,21 @@ class railsdev {
   class { 'rvm':
     system_users  => ['vagrant'],
   } ->
-  rvm_system_ruby { 'ruby-2.1.1':
+  rvm_system_ruby { 'ruby-2.2.5':
     ensure => present,
     default_use => true,
   }
 
   rvm_gemset {
-    'ruby-2.1.1@railsdev':
+    'ruby-2.2.5@railsdev':
       ensure  => present,
-      require => Rvm_system_ruby['ruby-2.1.1'];
+      require => Rvm_system_ruby['ruby-2.2.5'];
   }
 
   rvm_gem {
-    'ruby-2.1.1@railsdev/rails':
-      ensure  => '4.1',
-      require => Rvm_gemset['ruby-2.1.1@railsdev'];
+    'ruby-2.2.5@railsdev/rails':
+      ensure  => '5.0.0',
+      require => Rvm_gemset['ruby-2.2.5@railsdev'];
   }
 
   require postgresql::server
@@ -39,9 +39,20 @@ class railsdev {
     ensure => 'installed',
   }
 
+  yumrepo { 'epel':
+    mirrorlist     => 'https://mirrors.fedoraproject.org/mirrorlist?repo=epel-7&arch=\$basearch'
+    baseurl        => 'absent',
+    failovermethod => 'priority',
+    enabled        => true,
+    gpgcheck       => false,
+    gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7",
+    descr          => "Extra Packages for Enterprise Linux 7 - \$basearch",
+  } ->
   package { 'nodejs':
     ensure => 'installed',
   }
+
+  
 }
 
 include railsdev
